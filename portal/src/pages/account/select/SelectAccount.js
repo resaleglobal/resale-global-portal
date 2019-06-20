d5a2086a461392e5a61179a0a685c5ef067423b4
Kevyn Hale <kevyn@kevynhale.com>
2019-06-20 16:50:15 -0600

    setup account select page

--- portal/src/pages/account/select/SelectAccount.js
+++ portal/src/pages/account/select/SelectAccount.js
@@ -5,9 +5,28 @@ import { getValidDomains } from "../../../store/accounts/UserAccountsSelectors";
 import { selectAccount } from "../../../store/accounts/UserAccountsActions";
 import { Redirect } from "react-router-dom";
 
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+
+import { isAccountSelected } from "../../../store/accounts/UserAccountsSelectors";
+
 class SelectAccountPage extends Component {
+  selectAccount = params => {
+    this.props.selectAccount(params);
+  };
+
   render() {
-    const { getValidDomains, consignors, resellers } = this.props;
+    const {
+      getValidDomains,
+      consignors,
+      resellers,
+      domain,
+      isAccountSelected
+    } = this.props;
+
     if (getValidDomains.length === 1) {
       let account;
 
@@ -22,9 +41,47 @@ class SelectAccountPage extends Component {
       return <Redirect to={url} />;
     }
 
+    if (isAccountSelected) {
+      return <Redirect to={`/${domain}`} />;
+    }
+
     return (
       <AppNonAccountBody header="Select Account">
-        Select Account
+        <Table>
+          <TableHead className="table-head">
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Domain</TableCell>
+              <TableCell>Type</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {resellers.map(row => (
+              <TableRow
+                className="account-select-row"
+                key={row.id}
+                onClick={() => this.selectAccount({ ...row, type: "RESELLER" })}
+              >
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.domain}</TableCell>
+                <TableCell>RESELLER</TableCell>
+              </TableRow>
+            ))}
+            {consignors.map(row => (
+              <TableRow
+                className="account-select-row"
+                key={row.id}
+                onClick={() =>
+                  this.selectAccount({ ...row, type: "CONSIGNOR" })
+                }
+              >
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.domain}</TableCell>
+                <TableCell>CONSIGNOR</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </AppNonAccountBody>
     );
   }
@@ -33,7 +90,9 @@ class SelectAccountPage extends Component {
 const mapStateToProps = state => ({
   getValidDomains: getValidDomains(state),
   consignors: state.userAccount.consignors,
-  resellers: state.userAccount.resellers
+  resellers: state.userAccount.resellers,
+  isAccountSelected: isAccountSelected(state),
+  domain: state.userAccount.selected.domain
 });
 
 const mapDispatchToProps = dispatch => {
